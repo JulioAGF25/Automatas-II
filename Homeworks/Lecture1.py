1f2317f56e531c30fae0dd352b507f229cec1dc3
JulioAGF25 <L20760565@ensenada.tecnm.mx>
2023-02-28 19:17:30 -0800

    Now the code also prints the valid words that are repeated

--- Homeworks/Lecture1.py
+++ Homeworks/Lecture1.py
@@ -2,14 +2,22 @@ file = open("file.txt", "r")
 string = file.read()
 words = string.split()
 count = 0
-
+repeatedWords = {}
 while len(words) > count: 
     #read by character
     char = words[count]
     if not char: #if char is false finish the while | Si no es un char termina el ciclo
         break
     if char.isalpha():
+        char = char.lower()
+        if char in repeatedWords:
+            repeatedWords[char] += 1
+        else:
+            repeatedWords[char] = 1
         print(char, end=", ")
-        count = count + 1
-    else: count = count + 1
+        count += 1
+    else: count += 1
+for word, repeat in repeatedWords.items():
+    if repeat > 1:
+        print(word+':', repeat)
 file.close()
